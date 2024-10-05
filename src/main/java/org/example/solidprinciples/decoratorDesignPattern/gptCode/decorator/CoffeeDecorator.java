0844d5423aaedf82680bfa77d0519619f347f4a9
aasim khan <savdhanrahesatarkrahe@gmail.com>
2024-10-05 16:27:20 +0530

    Added Decorator Design Pattern (chatgpt prompt code)

---
+++ src/main/java/org/example/solidprinciples/decoratorDesignPattern/gptCode/decorator/CoffeeDecorator.java
@@ -0,0 +1,6 @@
+package org.example.solidprinciples.decoratorDesignPattern.gptCode.decorator;
+
+import org.example.solidprinciples.decoratorDesignPattern.gptCode.base.Coffee;
+
+public abstract class CoffeeDecorator implements Coffee {
+}
