68ff36b1d16b6ad3acf7bb404effd834b69809ff
aasimalikhan <72623216+aasimalikhan@users.noreply.github.com>
2024-11-30 15:37:58 +0530

    Chain of Responsibility Pattern - upadted

--- src/main/java/org/example/solidprinciples/chainOfResponsibilityPattern/code/chainOfResponsibilityPatternCode.java
+++ src/main/java/org/example/solidprinciples/chainOfResponsibilityPattern/code/chainOfResponsibilityPatternCode.java
@@ -2,7 +2,7 @@ package org.example.solidprinciples.chainOfResponsibilityPattern.code;
 
 public class chainOfResponsibilityPatternCode {
     public static void main(String[] args) {
-        Logger logger = new InfoLogger(new DebugLogger(new ErrorLogger(null)));
-
+        Logger logger = (new DebugLogger(new ErrorLogger(new InfoLogger(null))));
+        logger.log(1, "REST API response from localhost:3000/users loaded");
     }
 }
