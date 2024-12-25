b4a775edbb642e6122d659cd69fab9e932ff6278
aasimalikhan <72623216+aasimalikhan@users.noreply.github.com>
2024-12-25 17:43:52 +0530

    Design Authentication and Authorization System

---
+++ src/main/java/org/example/designScenarios/designAuthenticationAndAuthorization/code/AuthenticationService.java
@@ -0,0 +1,22 @@
+package org.example.designScenarios.designAuthenticationAndAuthorization.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthenticationService {
+    private Map<String, User> userStore = new HashMap<>();
+    public void registerUser(User user)
+    {
+        userStore.put(user.getUsername(), user);
+    }
+
+    public User authenticate(String username, String password)
+    {
+        User user = userStore.get(username);
+        if(user != null && user.getPassword().equals(password))
+        {
+            return user;
+        }
+        throw new RuntimeException("Invalid Credentials");
+    }
+}
